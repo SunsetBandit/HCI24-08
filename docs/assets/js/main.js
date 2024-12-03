0fee738b8b195d9b6e3a2a75e5edfca8139db78f
Michele Dussin <87281404+Duss02@users.noreply.github.com>
2024-12-03 23:48:56 +0100

    FINAL FLAAAASH

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -30,4 +30,64 @@ function updateSwitch(position) {
   });
 }
 
+const slider = document.querySelector(".items");
+const slides = document.querySelectorAll(".item");
+const button = document.querySelectorAll(".buttondx");
+
+let current = 0;
+let prev = 3;
+let next = 1;
+
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", () => gotoNext());
+}
+
+const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+
+const gotoNext = () => current < 2 ? gotoNum(current + 1) : gotoNum(0);
+
+const gotoNum = number => {
+  current = number;
+  prev = current - 1;
+  next = current + 1;
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+    slides[i].classList.remove("prev");
+    slides[i].classList.remove("next");
+  }
+
+  if (next == 3) {
+    next = 0;
+  }
+
+  if (prev == -1) {
+    prev = 3;
+  }
+
+  slides[current].classList.add("active");
+  slides[prev].classList.add("prev");
+  slides[next].classList.add("next");
+}
+const git = document.querySelectorAll(".github");
+const figma1 = document.querySelectorAll(".figma1");
+const figma2 = document.querySelectorAll(".figma2");
+
+git.forEach(item => {
+  item.addEventListener("click", () => {
+    window.open("https://github.com/SunsetBandit/HCI24-08", "_blank"); 
+  });
+});
+
+figma1.forEach(item => {
+  item.addEventListener("click", () => {
+    window.open("https://github.com/SunsetBandit/HCI24-08/tree/main/consegne", "_blank"); 
+  });
+});
+
+figma2.forEach(item => {
+  item.addEventListener("click", () => {
+    window.open("https://www.figma.com/proto/OghGLXV4DjVX6F6Sr3nEMR/Prototipi-Low%2FMid-Fidelity?node-id=973-4804&t=QJWHWKJavsn8p5hF-1", "_blank");
+  });
+});
 
